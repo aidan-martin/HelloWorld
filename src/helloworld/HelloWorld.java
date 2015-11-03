32241b4fa5022290a7d0f0996eba3513d62d4a6a
aidan-martin <aidan.martin@brentwood.ca>
2015-11-03 10:29:46 -0800

    init

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,77 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package helloworld;
+//
+//import javax.swing.JOptionPane;
+//
+///**
+// *
+// * @author aidanmartin
+// */
+//public class HelloWorld {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+////         TODO code application logic here
+////        System.out.println("Samuel is really mean to me.");
+////        System.out.println("I don't like mean people.");
+////        System.out.println("Samuel isn't actually mean, he is a very nice boy.");
+////        
+////        JOptionPane.showMessageDialog(null, "Aidan's computer works now!!!");
+////        String gunType = JOptionPane.showInputDialog("What type of gun would you like?");
+////        System.out.println(gunType);
+//        
+//        System.out.println("The sum is " + addTwoDoubles(3.4, 6.9));
+//        System.out.println("The sum is " + addTwoDoubles(5.7, 2.6));
+//        System.out.println("The sum is " + addTwoDoubles(1.0, 3.5));
+////      
+//        System.out.println("The product is " + addTwoIntegers (2, 3));
+//        System.out.println("The product is " + addTwoIntegers (0, Integer.MAX_VALUE));
+//        System.out.println("The product is " + addTwoIntegers (1, Integer.MAX_VALUE));
+//        
+//        System.out.println("The result is " + divideTwoIntegers (6, 3));
+//        System.out.println("The result is " + divideTwoIntegers (4, 2));
+//        System.out.println("The result is " + divideTwoIntegers (10, 5));
+//        System.out.println("The result is " + divideTwoIntegers (10, 2));
+//        System.out.println("The result is " + divideTwoIntegers (11, 2));
+//        
+//        System.out.println("The result is " + divideTwoDoubles(22, 11));
+//        System.out.println("The result is " + divideTwoDoubles(68, 34));
+//        System.out.println("The result is " + divideTwoDoubles(88, 44));
+//        System.out.println("The result is " + divideTwoDoubles(10, 2.0));
+//        System.out.println("The result is " + divideTwoDoubles(11, 2.0));
+//      
+//    }
+//  public static double addTwoDoubles (double number1, double number2){
+//      double result = number1 + number2;
+////      System.out.println("The result is " + result);
+//      return result;
+//   
+//  }
+//  
+//  public static int addTwoIntegers (int number1, int number2){
+//      int result = number1 * number2;
+//      return result;
+//  }
+//  
+//  public static int divideTwoIntegers (int number1, int number2){
+//      int result = number1 / number2;
+//              return result;
+//  }
+//  
+//  public static double divideTwoDoubles (double number1, double number2){
+//    double result = number1 / number2;
+//    return result;
+//}
+//
+//  
+//
+//  
+// }
+//
+// 
